52bda91f0a09c1d7c54e0f08cd0c6ae392ce3b07
Luka Lortkipanidze <89572091+l0rtk@users.noreply.github.com>
2023-08-06 15:43:05 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -3,7 +3,7 @@ use bubble_sort::bubble_sort;
 
 fn main() {
     let mut nums = [-9, 23, -32, 99, 12, 7];
-    println!("unsorted: {:?}", nums);
+    println!("Unsorted: {:?}", nums);
     bubble_sort(&mut nums);
-    println!("sorted: {:?}", nums);
+    println!("Sorted: {:?}", nums);
 }
